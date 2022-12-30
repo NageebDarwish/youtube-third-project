094a9662bcf1367182c2f30ff052179b1f59b22a
unknown <bravotech0sa@gmail.com>
2022-12-30 22:21:59 +0300

    upload users files

---
+++ Users/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function getAll()
+    {
+        $users = User::all();
+        return $users;
+    }
+    public function getbyId($id)
+    {
+        $obj = User::where('id', $id)->get();
+        return $obj;
+    }
+
+    public function remove(Request $request)
+    {
+        DB::table('users')->where('id', '=', $request->user_id)->delete();
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+        $user = User::where('email', $request->email)->first();
+        return $this->sucess([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of' . $user->name)->plainTextToken
+        ]);
+    }
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return $this->sucess([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of' . $user->name)->plainTextToken
+        ]);
+    }
+    public function logout()
+    {
+        return response()->json('This is my logout');
+    }
+}
---
+++ Users/routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Public Routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+
+// Users
+
+Route::prefix('user')->controller(AuthController::class)->group(function () {
+    Route::get('show', 'getAll');
+    Route::get('showbyid/{id}', 'getbyId');
+    Route::delete('delete/{user_id}', 'remove');
+});
+
+// Products
+
+Route::prefix('product')->controller(ProductsController::class)->group(function () {
+    Route::post('create', 'create');
+    Route::get('show', 'index');
+    Route::get('showbyid/{id}', 'getbyId');
+    Route::delete('delete/{id}', 'destroy');
+});
+
+
+
+// Protected Routes
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
